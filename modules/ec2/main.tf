8d47b3bf166ec619bf0b658f4c612118c08616ae
ANTHONY SAVARY <anthony.savary2003@gmail.com>
2025-07-31 15:27:45 +0200

    module ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "Instance1" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id                   = var.public_subnet_id
+  subnet_id                   = module.vpc.subnet_id.id
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  vpc_security_group_ids      = module.vpc.sg.id
 
   tags = {
     Name = "Instance1"
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -9,16 +9,3 @@ variable "ami_id"{
     type = string
     default = "ami-053b0d53c279acc90"
 }
-
-variable "web_sg_name" {
-    description = "Nom de groupe de securite de l'instance"
-    type = string
-    default = "webserver_sg"
-}
-
-variable "public_subnet_id" {
-  description = "ID du sous-reseau"
-  type = string
-  default = "subnet-07ef2b248305ca54c"
-
-}
