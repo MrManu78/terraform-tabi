53b228b4beaf00c4d5bbd0ebfa39864c7d7b8073
ANTHONY SAVARY <anthony.savary2003@gmail.com>
2025-07-31 16:04:11 +0200

    module ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "Instance1" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id                   = module.vpc.subnet_id.id
+  subnet_id                   = module.vpc.subnet_id
   associate_public_ip_address = true
-  vpc_security_group_ids      = module.vpc.sg.id
+  vpc_security_group_ids      = module.vpc_security_group.id
 
   tags = {
     Name = "Instance1"
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,11 +1,9 @@
 variable "instance_type" {
     description = "type d'instance EC2"
     type = string 
-    default = "t3.micro"
 }
 
 variable "ami_id"{
     description = "ID de l'image AMI"
     type = string
-    default = "ami-053b0d53c279acc90"
 }
