f75c3860168719038343398df88784cafa77bcb8
jenkins <emmanuel.lucheemeenen78@gmail.com>
2025-07-31 16:47:54 +0200

    added subnet ouput

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 module "ec2" {
   source = "./modules/ec2"
 
-  ami_id        = var.ami_id
+  ami_id = var.ami_id
   instance_type = var.instance_type
+}
+module "vpc" {
+  source = "./modules/vpc"
 }
